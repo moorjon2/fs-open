be3d51c9f72d74467bf89944d02aefc73947f1c8
Michael Fernandez <mfernandezm85@gmail.com>
2025-05-09 13:57:22 -0400

    fixed some code indentation

--- part3/phonebook/backend/models/person.js
+++ part3/phonebook/backend/models/person.js
@@ -5,31 +5,31 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 if (!url) {
-    console.error('MONGODB_URI not defined in .env')
-    process.exit(1)
+  console.error('MONGODB_URI not defined in .env')
+  process.exit(1)
 }
 
 console.log('Connecting to MongoDB:', url)
 
 mongoose.connect(url)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(error => console.error('MongoDB connection error:', error.message))
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(error => console.error('MongoDB connection error:', error.message))
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true,
-    },
-    number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: String,
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
