6804f9206dd47c818f442c8c291eedd29eacc256
Michael Fernandez <mfernandezm85@gmail.com>
2025-05-11 12:23:05 -0400

    changed backend project structure

---
+++ part4/notes/frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16,
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Oscura.engineering 2025
+      </em>
+    </div>
+  )
+}
+
+export default Footer
